b70abe664921bfa26a9cec6edd81e58f1449cb05
blueplanet <erguolinge@gmail.com>
2017-04-20 20:11:14 +0900

    hello

--- js/all.js
+++ js/all.js
@@ -1,5 +1,18 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>hello</div>
+    );
+  }
+}
+
 function init() {
-  console.log('react component mount');
+  const mountNode = document.getElementById("app");
+
+  ReactDOM.render(<Hello />, mountNode);
 }
 
 document.addEventListener("DOMContentLoaded", init);
