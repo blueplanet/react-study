8fd16446af3a76a22a2fc4f117663b8d449a839a
blueplanet <erguolinge@gmail.com>
2017-04-26 19:57:35 +0900

    todo

--- 2017-04-26/src/App.js
+++ 2017-04-26/src/App.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// cssをinputできるのは、webpackやってくれている
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      text: '',
+    };
+  }
+
   render() {
+
+    // const items = this.state.items;
+    const { items, text } = this.state;
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <ul>
+          <li>task1</li>
+          <li>task2</li>
+          <li>task3</li>
+        </ul>
+        <div>
+          <input type='text' value={text} onChange={(e) => this.setState({ text: e.target.value })} />
+          <input type='submit' value="Add" onClick={this.onClickAddButton.bind(this)} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
+  }
+
+  onClickAddButton() {
+    const { items, text } = this.state;
+    if ( text == null || text.length == 0) { return; }
+
+    // 新しい配列にする
+    const newItems = items.concat([text]);
+    this.setState({ items: newItems });
   }
 }
 
